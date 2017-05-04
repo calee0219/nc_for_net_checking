15baa7bd3db96ac85de5a5918387d2f7908113cb
Gavin Lee <sz110010@gmail.com>
2017-05-05 03:56:44 +0800

    Change output style of date

--- ex.sh
+++ ex.sh
@@ -6,9 +6,9 @@ source config
 #server_port=1234
 #wait_time=2
 
-date +%H:%M:%S > ${info}
-ifconfig | grep -v 127 | grep 'inet addr' | awk '{print $2}' | sed 's/addr:/from: /g' >> ${info}
-echo "" >> ${info}
+DATE=$(date +%H:%M:%S)
+IP=$(ifconfig | grep -v 127 | grep 'inet ' | awk '{print $2}' | sed 's/addr:/from: /g')
+echo "${DATE} ---- ${IP}" > ${info}
 sleep ${wait_time}
 nc -v ${server_ip} ${server_port} < ${info} &
 kill $(ps -a | grep 'nc -v' | awk '{print $1}')
