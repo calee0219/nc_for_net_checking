6629b893219ec7d0a258c20e373d829a13add5c4
Gavin Lee <sz110010@gmail.com>
2017-05-05 04:26:55 +0800

    RM from ip's 'from'

--- ex.sh
+++ ex.sh
@@ -7,7 +7,7 @@ source config
 #wait_time=2
 
 DATE=$(date +%H:%M:%S)
-IP=$(ifconfig | grep -v 127 | grep 'inet ' | awk '{print $2}' | sed 's/addr:/from: /g')
+IP=$(ifconfig | grep -v 127 | grep 'inet ' | awk '{print $2}' | sed 's/addr://g')
 echo "${DATE} ---- ${IP}" > ${info}
 sleep ${wait_time}
 nc -v ${server_ip} ${server_port} < ${info} &
